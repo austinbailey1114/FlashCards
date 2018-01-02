d9903d46f886f970e3fcd21bf287acc54cbb81aa
Austin Bailey <baileyau@bc.edu>
2018-01-02 15:33:42 -0500

    css on login page

--- login.php
+++ login.php
@@ -8,14 +8,20 @@ require './core/init.php';
 <html>
 <head>
 	<title>Login</title>
-	<style type="text/css" src="./css/loginStyle.css"></style>
+	<link rel="stylesheet" type="text/css" href="./css/login.css">
 </head>
 <body>
-	<form action="./api/checklogin.php" method="post">
-		<input type="text" name="username" placeholder="Username">
-		<input type="password" name="password" placeholder="password">
-		<button></button>
-	</form>
-	<a href="./newUser.php">New User</a>
+	<div>
+	<div id="centerDiv">
+		<div id="colorDiv"></div>
+		<form action="./api/checklogin.php" method="post" id="form">
+			<p></p>
+			<input type="text" name="username" placeholder="Username" id="usernameInput">
+			<input type="password" name="password" placeholder="password" id="passwordInput">
+			<button id="login">Log In</button>
+		</form>
+	</div>
+	<a href="./newUser.php" id="newUser">New User</a>
+	</div>
 </body>
 </html>
