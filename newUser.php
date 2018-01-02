a3db1341e1ef82cc4f469fb28d07d01349b82e87
Austin Bailey <baileyau@bc.edu>
2018-01-02 15:39:30 -0500

    added css to new user page

--- newUser.php
+++ newUser.php
@@ -6,15 +6,17 @@
 <html>
 <head>
 	<title>Create Account</title>
+	<link rel="stylesheet" type="text/css" href="./css/newUser.css">
 </head>
 <body>
-
-	<form action="./api/insertUser.php" method="post">
-		<input type="text" name="username">
-		<input type="password" name="password">
-		<input type="text" name="email">
-		<button>Sumbit</button>
+	<div id="centerDiv">
+		<div id="colorDiv"></div>
+		<form action="./api/insertUser.php" method="post">
+			<input type="text" name="username" id="usernameInput" placeholder="Username">
+			<input type="password" name="password" id="passwordInput" placeholder="Password">
+			<input type="text" name="email" id="emailInput" placeholder="Email">
+			<button id="submit">Sumbit</button>
 	</form>
-	
+	</div>
 </body>
 </html>
