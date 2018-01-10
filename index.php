245a8a8b834df4fa97c9c6ffbedb2ba4bed28496
Austin Bailey <baileyau@bc.edu>
2018-01-09 20:05:21 -0500

    temp

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
 require './core/init.php';
+
+$app = new \Slim\App;
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+	echo "hello";
+ 	
+});
+$app->run();
 
 $id = $_SESSION['id'];
 
